6f0d605f5d7d65ebd1a207db1f6e4d07e8bd36c7
Liam Campbell <liam22campbell@gmail.com>
2022-06-13 14:39:59 -0700

    Create TaskList and Tasks in js/scripts.
    Co-Authored-By: Kai Kholsdorf <kai.w.kholsdorf@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function TaskList() {
+	this.tasks = {}
+	this.currentNumber = 0;
+}
+
+TaskList.prototype.assignNumber = function() {
+	this.currentNumber +=1;
+	return this.currentNumber;
+};
+
+TaskList.prototype.addTask = function(task) {
+	task.number = this.assignNumber();
+	this.tasks[task.number] = task;
+};
+
+TaskList.prototype.findTask = function(number) {
+	if (this.task.number != undefined) {
+		return this.tasks[number];
+	}
+	return false;	
+};
+
+TaskList.prototype.deleteTask = function (number) {
+	if(this.tasks[number] === undefined) {
+		return false;
+	}
+	delete this.tasks[number];
+	return true;
+};
+
+function Task(nameOfTask) {
+	this.nameOfTask = nameOfTask;
+}
+
